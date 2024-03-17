d72dbd8b5739ebf9d00edab8abfb0839e7633062
Cooper Bodman <cooperjack23@gmail.com>
2024-03-16 22:43:18 -0700

    End of section 8

---
+++ NZWalks/NZWalks.API/Data/NZWalksAuthDbContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace NZWalks.API.Data
+{
+    public class NZWalksAuthDbContext : IdentityDbContext
+    {
+        public NZWalksAuthDbContext(DbContextOptions<NZWalksAuthDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            var readerRoleId = "72785587-6fb8-4ebb-9a6a-738a66c67bed";
+            var writerRoleId = "57f4e8a5-f18b-435e-97fc-b1cef2ce651a";
+
+            var roles = new List<IdentityRole>
+            {
+                 new IdentityRole
+                {
+                   Id = readerRoleId,
+                   ConcurrencyStamp = readerRoleId,
+                   Name = "Reader",
+                   NormalizedName = "Reader".ToUpper(),
+                },
+                 new IdentityRole
+                 {
+                     Id = writerRoleId,
+                     ConcurrencyStamp = writerRoleId,
+                     Name = "Writer",
+                     NormalizedName = "Writer".ToUpper()
+                 }
+            };
+
+            builder.Entity<IdentityRole>().HasData(roles);
+        }
+    }
+}
---
+++ NZWalks/NZWalks.API/Repositories/Interface/ITokenRepository.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace NZWalks.API.Repositories.Interface
+{
+    public interface ITokenRepository
+    {
+
+       string CreateJWTToken(IdentityUser user, List<string> roles);
+    }
+}
